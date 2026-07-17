8d734e2fed8719f1d1031c94609fad7c575a3745
evkaal <97579932+evkaal@users.noreply.github.com>
2026-07-17 07:34:11 -0600

    Update connection comment and variable name

--- models/conexion.php
+++ models/conexion.php
@@ -1,5 +1,5 @@
 <?php
-// Conexión al usuario dentro de la VPS
+// Conexión al VPS
 $conexion = new mysqli("localhost", "eduardo_bd", "4h91", "lavanderia_bd");
 //$conexion = new mysqli("localhost", "root", "", "JME_LAV");
 $conexion->set_charset("utf8");
